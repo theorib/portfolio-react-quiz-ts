4c3b770746c586a076ad5aa1faeb48bc3a2c2aa4
Theo Ribeiro <10909928+theorib@users.noreply.github.com>
2024-03-23 12:55:54 +0000

    make styles mobile first

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,8 +1,15 @@
+import { useQuiz } from '../contexts/QuizContext';
+
 function Header() {
+  const { dispatch } = useQuiz();
+
+  const handleClick = () => dispatch({ type: 'restart' });
   return (
-    <header className="app-header">
-      <img src="../logo512.png" alt="React logo" />
-      <h1>The React Quiz</h1>
+    <header className="header">
+      <button className="header-title" onClick={handleClick}>
+        <img src="../logo512.png" alt="React logo" />
+        <h1>The React Quiz</h1>
+      </button>
     </header>
   );
 }
